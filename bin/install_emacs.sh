7af960830da753a1c21eca6060d0da71df7664cf
Matthias Paulmier <hello@mattplm.fr>
2026-08-11 21:17:44 +0200

    Remove unused scripts

--- bin/install_emacs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-./configure --with-cairo --with-modules --without-compress-install --with-x-toolkit --with-gnutls --without-gconf --with-xwidgets --with-toolkit-scroll-bars --without-xaw3d --without-gsettings --with-mailutils --with-native-compilation --with-json --with-harfbuzz --without-imagemagick --with-jpeg --with-png --with-rsvg --with-tiff --with-wide-int --with-xft --with-xml2 --with-xpm CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer" prefix=/usr/local
-
-make
-
-sudo make install
